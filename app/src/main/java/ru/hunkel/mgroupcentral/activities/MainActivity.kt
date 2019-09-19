35ddd1a7771cdd759c82914834998d39ff62d060
hunkel <kolan19988@yandex.ru>
2019-09-19 17:26:07 +0500

    add open module settings

--- app/src/main/java/ru/hunkel/mgroupcentral/activities/MainActivity.kt
+++ app/src/main/java/ru/hunkel/mgroupcentral/activities/MainActivity.kt
@@ -1,6 +1,9 @@
 package ru.hunkel.mgroupcentral.activities
 
+import android.content.ComponentName
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -42,7 +45,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private class AppListAdapter(private var moduleList: MutableList<Module>) :
+    private inner class AppListAdapter(private var moduleList: MutableList<Module>) :
         RecyclerView.Adapter<AppListViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppListViewHolder {
             val view = LayoutInflater.from(parent.context)
@@ -63,13 +66,30 @@ class MainActivity : AppCompatActivity() {
             moduleList = list.toMutableList()
             notifyDataSetChanged()
         }
+
+        fun getItems() = moduleList
     }
 
-    private class AppListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private inner class AppListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         fun bind(module: Module) {
-            itemView.app_id_text_view.text = "id: ${module.id}"
-            itemView.app_package_text_view.text = "package: ${module.appPackage}"
-            itemView.app_settings_text_view.text = "settings: ${module.appSettings}"
+            itemView.module_id_text_view.text = "id: ${module.id}"
+            itemView.module_package_text_view.text = "${module.appPackage}"
+            itemView.module_settings_text_view.text = "${module.appSettings}"
+            itemView.module_settings_button.setOnClickListener {
+                try {
+                    val module = mAppAdapter.getItems()[adapterPosition]
+
+                    val intent = Intent()
+                    intent.component = ComponentName(
+                        module.appPackage,
+                        module.appSettings
+                    )
+                    startActivity(intent)
+                } catch (ex: Exception) {
+                    Log.e(TAG, ex.message)
+                }
+
+            }
         }
     }
 
