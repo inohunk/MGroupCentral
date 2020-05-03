2f5369f762267282108fa4d119a0138ab6cb341a
inohunk <inohunk@gmail.com>
2020-05-03 14:11:08 +0300

    add button listeners

--- app/src/main/java/ru/hunkel/mgroupcentral/activities/MainActivity.kt
+++ app/src/main/java/ru/hunkel/mgroupcentral/activities/MainActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.hunkel.mgroupcentral.R
 
 const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+    private var mIsTracking = false
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_overflow_menu, menu)
@@ -32,5 +34,49 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setButtonListeners()
+    }
+
+    private fun setButtonListeners() {
+        start_stop_button.setOnClickListener {
+            changeTrackingState()
+            updateUI()
+        }
+    }
+
+    private fun changeTrackingState() {
+        if (mIsTracking) {
+            if (use_bluetooth_tracking_checkbox.isChecked) {
+
+            }
+        } else {
+
+        }
+        mIsTracking = !mIsTracking
+    }
+
+    private fun startOGPSTrackerService() {
+
+    }
+
+    private fun stopOGPSTrackerService() {
+
+    }
+
+    private fun startMGroupTrackerService() {
+
+    }
+
+    private fun stopMGroupTrackerService() {
+
+    }
+
+    private fun updateUI() {
+        if (mIsTracking) {
+            start_stop_button.text = "Stop"
+        } else {
+            start_stop_button.text = "Start"
+        }
     }
 }
